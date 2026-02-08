4d8e60e4a9fb151e30bd4df369dfe75699ff4971
GIOS-Cervecería <hielo.torres.ice@gmail.com>
2026-02-08 20:21:32 -0300

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -1,1 +1,21 @@
+function agregarProducto(id) {
+  const prod = productos.find(p => p.id === id);
+  if (prod) prod.qty++;
+  actualizarSubtotal();
+}
 
+function quitarProducto(id) {
+  const prod = productos.find(p => p.id === id);
+  if (prod && prod.qty > 0) prod.qty--;
+  actualizarSubtotal();
+}
+
+function calcularSubtotal() {
+  return productos.reduce((sum, p) => sum + p.precio * p.qty, 0);
+}
+
+function actualizarSubtotal() {
+  const subtotal = calcularSubtotal();
+  document.getElementById("subtotalValue").innerText =
+    subtotal > 0 ? `Ir a pagar $ ${subtotal.toLocaleString()}` : "Ir a pagar $ 0";
+}
