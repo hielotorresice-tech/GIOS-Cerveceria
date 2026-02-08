5154b52d59bf23d6eaee50e0435759718e72e2d1
GIOS-Cervecería <hielo.torres.ice@gmail.com>
2026-02-08 20:27:48 -0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,8 @@
-
+function abrirResumenPedido(){
+  const items = productos.filter(p => p.qty > 0);
+  if(items.length === 0){
+    alert("Agrega productos antes de continuar");
+    return;
+  }
+  document.getElementById("checkout").style.display = "block";
+}
